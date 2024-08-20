7713411bfda75cd9489712c2d108e8f30d125b90
staneaurelius <staneaurelius@gmail.com>
2024-08-20 17:55:09 +0700

    Initiate restaurant project - Configure webpack - Structure src dir

---
+++ 05-restaurant/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+        clean: true
+    },
+    devtool: 'eval-source-map',
+    devServer: {
+        watchFiles: ['./src/index.html']
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
