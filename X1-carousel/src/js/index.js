5a989e0b08353005fe2e6d0042de92dc4330ccef
staneaurelius <staneaurelius@gmail.com>
2024-08-30 19:25:07 +0700

    Enable dropdown - Add hidden dropdown - Add logic to display dropdown and change value

--- X1-carousel/src/js/index.js
+++ X1-carousel/src/js/index.js
@@ -1,2 +1,19 @@
-import "../css/normalize.css"
-import "../css/style.css"
+import "../css/normalize.css";
+import "../css/style.css";
+
+const dropdownHandler = (function () {
+  const dropdownBtn = document.querySelector(".dropdown"),
+    dropdownContainer = document.querySelector('.dropdown-items'),
+    dropdownItems = document.querySelectorAll('li');
+
+  dropdownBtn.addEventListener("click", (e) => {
+    dropdownContainer.classList.toggle('hidden');
+  });
+
+  dropdownItems.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        dropdownBtn.textContent = e.target.textContent;
+        dropdownContainer.classList.toggle('hidden');
+    });
+  });
+})();
