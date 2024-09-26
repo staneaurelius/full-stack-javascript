60bfd14a8b76da6c2d4ee6f7a9e8f5654e85278d
staneaurelius <staneaurelius@gmail.com>
2024-09-26 11:12:05 +0700

    Initiate battleship project

---
+++ 14-battleship/webpack.common.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry : './src/js/index.js',
+    output : {
+        path : path.resolve(__dirname, 'dist'),
+        filename : 'main.js',
+        clean : true
+    },
+    plugins : [
+        new HtmlWebpackPlugin({ template : './src/template.html' })
+    ],
+    module : {
+        rules : [
+            {
+                test : /\.css$/i,
+                use : ['style-loader', 'css-loader']
+            }
+        ]
+    }
+};
